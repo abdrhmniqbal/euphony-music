93e8d6add73e24b5a0fd8a0b26e337271190cd51
abdrhmniqbal <iqbalabdurrahman04@gmail.com>
2026-08-24 12:30:38 +0700

    chore: regenerate icons and uniwind types

--- src/components/icons/local/add-01.tsx
+++ src/components/icons/local/add-01.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import { SvgXml, type SvgProps } from "react-native-svg";
 
 const LocalAdd01Icon = (props: Omit<SvgProps, "xml">) => {
-  const xml = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12.001 5.00003V19.002" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M19.002 12.002L4.99998 12.002" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`;
+  const xml = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.001 5.00003V19.002" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M19.002 12.002L4.99998 12.002" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
 
   return <SvgXml xml={xml} {...props} />;
 };
